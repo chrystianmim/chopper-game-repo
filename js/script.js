e3c0d461cbaeed53b5aaddfdbaaff413b83f8119
chrystianmim <chrystianmim@gmail.com>
2022-04-18 21:25:25 -0300

    added some animation

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ function start() {
 
     $("#startGame").hide();
 
-    $("#background").append("<div id='player'></div>");
-    $("#background").append("<div id='enemy1'></div>");
+    $("#background").append("<div id='player' class='animation1'></div>");
+    $("#background").append("<div id='enemy1' class='animation2'></div>");
     $("#background").append("<div id='enemy2'></div>");
-    $("#background").append("<div id='friend'></div>");
+    $("#background").append("<div id='friend' class='animation3'></div>");
 
 }
 
