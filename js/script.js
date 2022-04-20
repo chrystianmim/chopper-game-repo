bb2d580a12de785529bda39624d041cee41060f7
chrystianmim <chrystianmim@gmail.com>
2022-04-19 22:12:48 -0300

    defined game keys variables, created functions to: move player and move enemy1

--- js/script.js
+++ js/script.js
@@ -8,6 +8,23 @@ function start() {
     $("#background").append("<div id='friend' class='animation3'></div>");
 
     const game = {};
+    const keys = {
+        w: 87,
+        s: 83,
+        d: 68
+    }
+    const enemy1Speed = 3;
+    let enemy1PosY = parseInt(Math.random() * 334);
+
+    game.keyPressed = []; // check if player pressed a key
+
+    $(document).keydown(function (e) {
+        game.keyPressed[e.which] = true;
+    })
+
+    $(document).keyup(function (e) {
+        game.keyPressed[e.which] = false;
+    })
 
     // Game Loop
 
@@ -15,11 +32,50 @@ function start() {
 
     function loop() {
         bgMovement();
+        movePlayer();
+        moveEnemy1();
     }
 
     function bgMovement() { // Function that moves the background
         left = parseInt($("#background").css("background-position"));
         $("#background").css("background-position", left - 1);
+    }
+
+    function movePlayer() { // #player moves (up, down, fire) function
+        if (game.keyPressed[keys.w]) {
+            let moveVert = parseInt($("#player").css("top"));
+            if (moveVert <= 10) {
+                return false
+            } else {
+                $("#player").css("top", moveVert - 10);
+            }
+        }
+
+        if (game.keyPressed[keys.s]) {
+            let moveVert = parseInt($("#player").css("top"));
+            if (moveVert >= 430) {
+                return false
+            } else {
+                $("#player").css("top", moveVert + 10);
+            }
+        }
+
+        if (game.keyPressed[keys.d]) {
+            // function fire
+        }
+    }
+
+    function moveEnemy1() {
+        let enemy1PosX = parseInt($("#enemy1").css("left"));
+
+        if (enemy1PosX <= 0) {
+            enemy1PosY = parseInt(Math.random() * 334);
+            $("#enemy1").css("left", 694);
+            $("#enemy1").css("top", enemy1PosY);
+        } else {
+            $("#enemy1").css("left", enemy1PosX - enemy1Speed);
+            $("#enemy1").css("top", enemy1PosY);
+        }
     }
 }
 
