79a78d56a949f1024e184e9cbdac41238ba36e1d
chrystianmim <chrystianmim@gmail.com>
2022-04-20 05:58:05 -0300

    added function to fire

--- js/script.js
+++ js/script.js
@@ -13,7 +13,10 @@ function start() {
         s: 83,
         d: 68
     }
-    const enemy1Speed = 3;
+    const enemy1Speed = 3.5;
+    const enemy2Speed = 1.25;
+    const friendSpeed = 1;
+    let letPlayerFire = true;
     let enemy1PosY = parseInt(Math.random() * 334);
 
     game.keyPressed = []; // check if player pressed a key
@@ -34,6 +37,8 @@ function start() {
         bgMovement();
         movePlayer();
         moveEnemy1();
+        moveEnemy2();
+        moveFriend();
     }
 
     function bgMovement() { // Function that moves the background
@@ -61,7 +66,7 @@ function start() {
         }
 
         if (game.keyPressed[keys.d]) {
-            // function fire
+            playerFire();
         }
     }
 
@@ -75,6 +80,57 @@ function start() {
         } else {
             $("#enemy1").css("left", enemy1PosX - enemy1Speed);
             $("#enemy1").css("top", enemy1PosY);
+        }
+    }
+
+    function moveEnemy2() {
+        let enemy2PosX = parseInt($("#enemy2").css("left"));
+
+        if (enemy2PosX <= 0) {
+            $("#enemy2").css("left", 775);
+        } else {
+            $("#enemy2").css("left", enemy2PosX - enemy2Speed);
+        }
+    }
+
+    function moveFriend() {
+        let friendPosX = parseInt($("#friend").css("left"));
+
+        if (friendPosX >= 694) {
+            $("#friend").css("left", 0);
+        } else {
+            $("#friend").css("left", friendPosX + friendSpeed);
+        }
+    }
+
+    function playerFire() {
+        if (letPlayerFire === true) {
+
+            letPlayerFire = false;
+
+            // bullet position
+
+            posY = parseInt($("#player").css("top"));
+            posX = parseInt($("#player").css("left"));
+            bulletX = posX + 190;
+            bulletY = posY + 37;
+            $("#background").append("<div id='bullet'></div>");
+            $("#bullet").css("top", bulletY);
+            $("#bullet").css("left", bulletX);
+
+            var fireTime = window.setInterval(fireMovement, 30)
+        }
+
+        function fireMovement() {
+            posX = parseInt($("#bullet").css("left"));
+            if (posX >= 900) {
+                window.clearInterval(fireTime);
+                fireTime = null;
+                $("#bullet").remove();
+                letPlayerFire = true;
+            } else {
+                $("#bullet").css("left", posX + 30);
+            }
         }
     }
 }
